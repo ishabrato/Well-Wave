85a0e9649f1a6362c2b739f258e1eaf0fdd1eebe
Smarak011 <smarak2003haldar@gmail.com>
2024-10-01 21:08:33 +0530

    commit Smarak 2

--- wellwave/src/Pages/Nav.jsx
+++ wellwave/src/Pages/Nav.jsx
@@ -1,50 +1,34 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Dropdown from 'react-bootstrap/Dropdown';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-import './App.css';
-function Nav(){
+import '../App.css';
+
+function NavBar(){
   return (
-    <div>
-            <div className='navbar'>
-      <Navbar expand="lg" className="fixed-top bg-body-tertiary shadow">
-        <Container>
-          <Navbar.Brand>
-            <Link to="/" className='navbar-brand text-success fw-semibold'>
-            Well WAVE
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-          <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto justify-content-end w-100'>
-            <Nav.Link href='/' className='active-text-uppercase'>Home</Nav.Link>
-            <Nav.Link href='/about' className='active-text-uppercase'>About</Nav.Link>
-            <Nav.Link > <Dropdown>
-      <Dropdown.Toggle >
-        Log in 
-      </Dropdown.Toggle>
+    <Navbar expand="lg" className="fixed-top bg-body-tertiary shadow">
+      <Container>
+        <Navbar.Brand>
+          <Link to="/" className='navbar-brand font-italic title text-success fw-semibold'>
+          Well WAVE
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+        <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='me-auto justify-content-end w-100 d-flex'>
+          <Nav.Link href='/' className='active-text-uppercase'>Home</Nav.Link>
+          <Nav.Link href='/about' className='active-text-uppercase'>About</Nav.Link>
+          <Nav.Link href='/login' className='active-text-uppercase'>Login</Nav.Link>
+          <Nav.Link href='/register' className='active-text-uppercase'>
+            Register
+          </Nav.Link>         
+        </Nav>
 
-      <Dropdown.Menu>
-        <Dropdown.Item ><Link to="/sadmin">Super Admin</Link></Dropdown.Item>
-        
-        <Dropdown.Item ><Link to="/admin"> Admin</Link></Dropdown.Item>
-
-        <Dropdown.Item ><Link to="/user"> User</Link></Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-      </Nav.Link>
-            
-          </Nav>
-
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-</div>
-
-    </div>
-  )
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Nav
+export default NavBar;
